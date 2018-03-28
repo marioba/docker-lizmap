d7a7eb602ad7a331962b1c5da4001a5c2f574fde
Mario Baranzini <mario@opengis.ch>
2018-03-28 09:01:53 +0200

    First commit Zermatt rd-gis project

--- files/lizmapConfig.ini.php
+++ files/lizmapConfig.ini.php
@@ -12,7 +12,7 @@ defaultProject=
 cacheStorageType=redis
 ;cacheStorageType=sqlite => store cached images in one sqlite file per repo/project/layer
 ;cacheStorageType=file => store cached images in one folder per repo/project/layer. The root folder is /tmp/
-cacheRedisHost=redis  
+cacheRedisHost=redis
 cacheRedisPort=6379
 cacheExpiration=0
 ; default cache expiration : the default time to live of data, in seconds.
@@ -23,7 +23,7 @@ proxyMethod=php
 debugMode=0
 ; debug mode
 ; on = print debug messages in lizmap/var/log/messages.log
-; off = no lizmap debug messages    
+; off = no lizmap debug messages
 cacheRootDirectory="/tmp/"
 ; cache root directory where cache files will be stored
 ; must be writable
@@ -35,7 +35,7 @@ allowUserAccountRequests=off
 ; relativeWMSPath=0
 
 
-appName=Demo
+appName=rdgis
 qgisServerVersion=2.18
 wmsMaxWidth=3000
 wmsMaxHeight=3000
@@ -43,8 +43,7 @@ relativeWMSPath=0
 cacheRedisDb=1
 cacheRedisKeyPrefix=a
 
-[repository:demo]
-label=demo
+[repository:rdgis]
+label=rd-gis
 path="/home/files/qgis/"
 allowUserDefinedThemes=1
-
